20fedd9295c9f03da7b4a2469d445b6085b34814
kingnov <mmuliro59@gmail.com>
2025-03-01 17:57:13 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='text-sm text-green-500'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
